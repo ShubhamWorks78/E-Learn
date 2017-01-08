d770b37e062978e29ad897c37f6344d6cc9c67c2
Chandra Sekhar <chandrasekhar.kohli@gmail.com>
2017-01-09 00:53:57 +0530

    Initial Commit
    App basic Layout 1,can be modified afterwards.

---
+++ app/src/main/java/com/mania/indiaelearn/tab3community.java
@@ -0,0 +1,22 @@
+package com.mania.indiaelearn;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mania on 08-01-2017.
+ */
+
+
+public class tab3community extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab3community, container, false);
+
+        return rootView;
+    }
+}
